3ef22207a2820f645b3eadc0a12981f317708567
fumi <fumifumi7629@gmail.com>
2022-10-28 23:22:55 +0900

    add todo CRUD function

--- api/main.go
+++ api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/fumi7649/go-react-todo/model"
 	"github.com/fumi7649/go-react-todo/utils"
 )
@@ -11,5 +13,5 @@ func init () {
 }
 
 func main () {
-
+	
 }
